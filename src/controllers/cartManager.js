edbdd226bfa4f76421e2e486e3f418aa11329c2a
pedrisdm87 <117787470+pedrisdm87@users.noreply.github.com>
2023-09-01 19:07:26 -0300

    Desafio 10 correccion 1

--- src/controllers/cartManager.js
+++ src/controllers/cartManager.js
@@ -1,108 +1,97 @@
-const socket = io();
+import fs from "fs";
 
-// apreton de manos, en el servidor ( app ) se llama io
-// y en el cliente lo llamo socket ( index.js )
+export default class CartManager {
+    // #error;
+    #path;
+    #format;
 
-const form = document.getElementById("form");
-const productsTable = document.querySelector("#productsTable");
-const tbody = productsTable.querySelector("#tbody");
+    constructor() {
+        this.#path = "./src/data/cart.json";
+        this.#format = "utf-8";
+        this.carts = [];
+        // this.#error = undefined;
+    }
 
-
-form.addEventListener("submit", async (evt) => {
-    evt.preventDefault();
-
-    let product = {
-        title: document.querySelector("#title").value,
-        description: document.querySelector("#description").value,
-        price: document.querySelector("#price").value,
-        code: document.querySelector("#code").value,
-        category: document.querySelector("#category").value,
-        stock: document.querySelector("#stock").value,
+    // Devolvemos el arreglo con todos los productos agregados al carrito. En caso de error lo mostramos por consola
+    getCarts = async () => {
+        try {
+            return JSON.parse(
+                await fs.promises.readFile(this.#path, this.#format)
+            );
+        } catch (error) {
+            console.log("error: archivo no encontrado");
+            return this.carts;
+        }
     };
 
-    const res = await fetch("/api/products", {
-        method: "POST",
-        body: JSON.stringify(product),
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
-
-    try {
-        const result = await res.json();
-        if (result.status === "error") {
-            throw new Error(result.error);
+    async #generateId() {
+        const carts = await this.getCarts();
+        if (carts && carts.length > 0) {
+            return carts[carts.length - 1].id + 1;
         } else {
-            const resultProducts = await fetch("/api/products");
-            const results = await resultProducts.json();
-            if (results.status === "error") {
-                throw new Error(results.error);
-            } else {
-                socket.emit("updatedProducts", results.products);
+            return 1;
+        }
+    }
 
-                Toastify({
-                    text: "New product added successfully",
-                    duration: 3000,
-                    gravity: "top",
-                    position: "right",
-                    style: {
-                        backgroundImage:
-                            "linear-gradient(to right, #0AD100, #37B300)",
-                        backgroundColor: "#37B300",
-                    },
-                    onClick: function () {},
-                }).showToast();
+    addCart = async (products) => {
+        const carts = await this.getCarts();
 
-                form.reset();
+        const newCart = {
+            id: await this.#generateId(),
+            products: (products = []),
+        };
+
+        carts.push(newCart);
+
+        // Escribir el array actualizado del carrito en el archivo
+        await fs.promises.writeFile(this.#path, JSON.stringify(carts, null, "\t"));
+
+        // Actualizo el array del carrito
+        this.carts = carts;
+
+        // Devolver el nuevo producto
+        return newCart;
+    };
+
+    async getCartById(id) {
+        try {
+            let allCarts = await this.getCarts();
+            let cart = allCarts.find((item) => item.id == id);
+            return cart
+        } catch (err) {
+            console.log("Hubo un error: " + err);
+        }
+    }
+
+    async addProductsToCart (cartId, productId) {
+        const carts = await this.getCarts();
+        // Busco el indice del carrito
+        const cartIndex = carts.findIndex((item) => item.id == cartId)
+        if (cartIndex == -1) {
+            return null // El carrito no existe
+        }
+
+        const cart = carts[cartIndex]
+
+        // Verifico si el producto existe
+        const productIndex = cart.products.findIndex((item) => item.product == productId)
+        // Si existe, actualizo la cantidad, si no existe, lo agrego al carrito
+        if (productIndex !== - 1){
+            cart.products[productIndex].quantity++
+        } else {
+            const newProduct = {
+                product: productId,
+                quantity: 1
             }
+            cart.products.push(newProduct)
         }
-    } catch (error) {
-        console.log(error);
+
+        // Actualizo el carrito
+        carts[cartIndex] = cart
+
+        // Guardo los cambios en el JSON del carrito
+        await fs.promises.writeFile(this.#path, JSON.stringify(carts, null, "\t"))
+
+        return carts
     }
-});
-
-const deleteProduct = async (id) => {
-    try {
-        const res = await fetch(`/api/products/${id}`, { method: "DELETE"});
-        const result = await res.json();
-        if (result.status === "error") {
-            throw new Error(result.error);
-        } else {
-            socket.emit("updatedProducts", result.products);
-
-            Toastify({
-                text: "Product removed successfully",
-                duration: 3000,
-                gravity: "top",
-                position: "right",
-                style: {
-                    background: "linear-gradient(to right, #FF0000, #FF2100)",
-                },
-                onClick: function(){},
-            }).showToast();
-        }
-    } catch (error) {
-        console.log(error);
-    }
-};
-
-socket.on("updatedProducts", (products) => {
-    console.log(products);
-    tbody.innerHTML = "";
-
-    products.forEach((item) => {
-        const row = document.createElement("tr");
-        row.innerHTML = `
-            <td>${item.title}</td>
-            <td>${item.description}</td>
-            <td>${item.price}</td>
-            <td>${item.code}</td>
-            <td>${item.category}</td>
-            <td>${item.stock}</td>
-            <td>
-                <button class="btn btn-danger" onclick="deleteProduct(${item.id})">❌</button>
-            </td>
-        `;
-        tbody.appendChild(row);
-    });
-});
+}
